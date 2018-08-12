563b464cffd5a5b441ecea3aec51017438817b94
YongJie-Xie <xyj@xieyongjie.cn>
2018-08-12 21:31:35 +0800

    提交写题模板
    所有的题目都拷贝这份模板进行作答。

---
+++ 2018-08/src/cn/xieyongjie/leetcode/Template.java
@@ -0,0 +1,27 @@
+package cn.xieyongjie.leetcode;
+
+/**
+ * 题目：
+ * <p>
+ * 示例：
+ * <p>
+ * 总结：
+ *
+ * @author YongJie-Xie
+ * @data 08/12/2018
+ */
+public class Template {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        // Show method result
+        new Template().method();
+        System.out.println();
+
+        System.out.println("执行时间：" + (System.currentTimeMillis() - startTime) + "ms");
+    }
+
+    private void method() {
+
+    }
+}
